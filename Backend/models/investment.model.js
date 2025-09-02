4ba5839bf04b91c8d08f9096b1c2752a096b5db9
Jamal670 <jamalobaid2@gmail.com>
2025-09-02 21:54:02 +0500

    after complete admin

---
+++ Backend/models/investment.model.js
@@ -0,0 +1,58 @@
+// investment.model.js
+const mongoose = require('mongoose');
+
+const investmentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', // Reference to User model
+      required: true,
+    },
+    reDepId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Investment', // Reference to Investment model
+      required: false,
+    },
+    exchangeType: {
+      type: String,
+      required: true,
+    },
+    ourExchange: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number, // use Number instead of String for amounts
+      required: true,
+      min: 0,
+    },
+    userExchange: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+      default: '', // optional
+    },
+    status: {
+      type: String,
+      enum: ['Confirmed', 'Pending', 'Declined'],
+      default: 'Pending',
+    },
+    type: {
+      type: String,
+      enum: ['Deposit', 'Withdraw'],
+      required: true,
+    },
+      comment: {
+    type: String,
+  },
+  },
+  { timestamps: true }
+);
+
+// module.exports = mongoose.models.Investment || mongoose.model('Investment', investmentSchema);
+module.exports = mongoose.model('Investment', investmentSchema);
+//helper function
